6e2ee3b24e8e194d47a8879b156a1a95f55cc190
mathias1000zzz <mathqrr@gmail.com>
2024-01-15 13:11:28 +0100

    increaseAndDecrease

---
+++ index.js
@@ -0,0 +1,18 @@
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 1;
+
+increaseBtn.onclick = function() {
+    count = 19;
+    countLabel.textContent = count;
+}
+decreaseBtn.onclick = function() {
+    count--;
+    countLabel.textContent = count;
+}
+resetBtn.onclick = function() {
+    count = 0;
+    countLabel.textContent = count;
+}
